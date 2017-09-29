567209095f1a3fa0757591179c90ba021f05badf
Kishore <kishore.iiitn@gmail.com>
2017-09-29 16:55:19 +0530

    Add routes to Register page and login for any unknown url.

--- App/index.tsx
+++ App/index.tsx
@@ -4,11 +4,9 @@ import {ClassUI} from 'classui/ClassUI';
 import {Content} from 'classui/Content';
 import {NavBar} from 'classui/Navbar';
 import {Login} from './Pages/Login';
-import {withRouter, BrowserRouter, Route} from 'react-router-dom';
-import * as io from 'socket.io-client';
-
-let Socket = io();
-
+import {Register} from './Pages/Register';
+import {withRouter, BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import {SocketIO} from './SocketIO';
 interface IProps {};
 export class App extends React.Component<IProps, any>
 {
@@ -18,7 +16,11 @@ export class App extends React.Component<IProps, any>
 			<ClassUI contentWidth={800}>
 				<NavBar fixed logo="Programmer's Club"></NavBar>
 				<Content>
+					<Switch>
 					<Route  path="/login" strict exact component={Login} />
+					<Route  path="/register" strict exact component={Register} />
+					<Route render={(props)=>{console.log(props);return <Redirect from="" to="/login"></Redirect>}} />
+					</Switch>
 				</Content>
 			</ClassUI>
 		</BrowserRouter>;
