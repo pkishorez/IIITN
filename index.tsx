b293fb59f6ba24de939059e8b7cdb8f8e15bec40
Kishore <kishore.iiitn@gmail.com>
2017-10-10 10:30:47 +0530

    Refactor.

--- index.tsx
+++ index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {ClassUI} from 'classui/ClassUI';
-import {Content} from 'classui/Content';
-import {NavBar} from 'classui/Navbar';
 import {App} from './App';
 
 ReactDOM.render(<App />, document.getElementById("app"));
