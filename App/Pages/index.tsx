2b772acc87ff9fe1992c6cfaf81e5c732125aebf
Kishore <kishore.iiitn@gmail.com>
2017-10-15 21:30:58 +0530

    Move login, register pages to Presentation folder.

---
+++ App/Pages/index.tsx
@@ -0,0 +1,4 @@
+export {Login} from './Presentation/Login';
+export {Register} from './Presentation/Register';
+export {Profile} from './Presentation/Profile';
+export {Students} from './Presentation/Students';
