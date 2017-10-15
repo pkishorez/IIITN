222143357693c634b9995ffc2b6379217e51bff2
Kishore <kishore.iiitn@gmail.com>
2017-10-15 18:01:02 +0530

    Display Login error message on error.

--- App/Pages/Login.tsx
+++ App/Pages/Login.tsx
@@ -8,37 +8,58 @@ import {RouteComponentProps, Redirect, Link} from 'react-router-dom';
 import {SLoginUser} from '../../Server/Database/Schema';
 import {User} from '../User';
 
-interface IProps extends RouteComponentProps<any> {
+interface IProps {
 	redirect?: string
 };
-interface IState {};
+interface IState {
+	error?: string
+};
 
 export class Login extends React.Component<IProps, IState> {
-	private dismiss: any;
+
 	render() {
 		return <div>Kishore is a good boy.</div>;
 	}
-	login(data: any) {
-		User.login(data).then(console.log, console.error);
-	}
+
 	componentDidMount() {
 		Flash.flash((dismiss)=>{
-			this.dismiss = dismiss;
-			return <div>
-				<Link to="/register"><div className="button">Register Here.</div></Link>
-				<Formlayout schema={SLoginUser} onSubmit={this.login.bind(this)} label="Login">
-					<TextField autoFocus name="_id" label="UserName (University ID)">Username</TextField>
-					<TextField type="password" name="password" label="Password">Password</TextField>
-					<input type="submit"/>
-				</Formlayout>
-			</div>
+			let cwu = this.componentWillUnmount;
+			this.componentWillUnmount = ()=>{
+				cwu?cwu.apply(this):null;
+				dismiss();
+			}
+			return <Login_ />;
 		},
 		true);
 	}
-	componentWillUnmount() {
-		if (this.dismiss) {
-			console.log("Dismissing...");
-			this.dismiss();
-		}
+}
+
+class Login_ extends React.Component<IProps, IState> {
+	constructor() {
+		super();
+		this.state = {
+			error: undefined
+		};
+	}
+	login(data: any) {
+		User.login(data).then((response: string)=>{
+			console.log(response);
+		}, (error: string)=>{
+			console.log(error);
+			this.setState({
+				error
+			});
+		});
+	}
+	render(){
+		return <div>
+			<Link to="/register"><div className="button">Register Here.</div></Link>
+			<Formlayout schema={SLoginUser} onSubmit={this.login.bind(this)} label="Login">
+				{this.state.error?<h5 style={{color: 'red'}}>{this.state.error}</h5>:null}
+				<TextField autoFocus name="_id" label="UserName (University ID)">Username</TextField>
+				<TextField type="password" name="password" label="Password">Password</TextField>
+				<input type="submit"/>
+			</Formlayout>
+		</div>;
 	}
 }
