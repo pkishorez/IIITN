ba259018af43664ce6c7ca787d812b1cee80c28c
Kishore <kishore.iiitn@gmail.com>
2017-10-16 20:37:19 +0530

    Display profile page of respective user, captured from url.

--- App/Pages/Presentation/Profile.tsx
+++ App/Pages/Presentation/Profile.tsx
@@ -3,19 +3,39 @@ import * as ReactDOM from 'react-dom';
 import {Layout, Section} from 'classui/Components/Layout';
 import {User} from '../../User';
 
-interface IProps {};
-interface IState {};
+interface IProps {
+	userid: string
+};
+interface IState {
+	profile: any
+};
 
 export class Profile extends React.Component<IProps, IState> {
+	constructor() {
+		super();
+		this.state = {
+			profile: null
+		};
+	}
 	componentDidMount() {
-		
+		User.getProfile(this.props.userid).then((profile)=>{
+			console.log(profile);
+			this.setState({
+				profile
+			});
+		}).catch((error)=>{
+			console.error(error);
+		});
 	}
 	render() {
-		return <Layout c_props={{card: true}} gutter={20} justify="center">
-			<Section width={600}>
-				<h4>Bala Kishore Polamarasetty</h4>
-				<h5>Age : 25</h5>
-				<h5>Class : E4 CSE-6</h5>
+		let {profile} = this.state;
+		if (!profile) {
+			return null;
+		}
+		return <Layout gutter={20} justify="center">
+			<Section width={600} card>
+				<h4>{profile.name}</h4>
+				<h5>Class : {profile.batch}-{profile.branch}</h5>
 				<h5>Learning Progress : 50%</h5>
 			</Section>
 		</Layout>;
