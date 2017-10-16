3d44e2a12d9b584a08bab17a46880248f4669bcf
Kishore <kishore.iiitn@gmail.com>
2017-10-16 11:29:28 +0530

    Handy History component, to access history props on fly.

---
+++ App/History.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Redirect, withRouter, RouteComponentProps} from 'react-router-dom';
+
+interface IProps {};
+interface IState {};
+
+export class History extends React.Component<IProps & RouteComponentProps<any>, IState> {
+	private static instance: History;
+
+	constructor() {
+		if (History.instance) {
+			console.error("Only one instance of History should be rendered.");
+			return;
+		}
+		super();
+		History.instance = this;
+	}
+	static get props() {
+		if (!this.instance) {
+			console.error("History component should be mounted First to access props.");
+			return null;
+		}
+		return this.instance.props;
+	}
+	render() {
+		return null;
+	}
+}
+
+export let History_ = withRouter<IProps>(History);
