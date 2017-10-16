237b71a6eda6b6ee068cb0dbffe601603817bb82
Kishore <kishore.iiitn@gmail.com>
2017-10-16 14:31:51 +0530

    Save redux state to localstorage every 2 seconds.

--- App/State/index.tsx
+++ App/State/index.tsx
@@ -1,4 +1,22 @@
 import {RootReducer, IRootState} from './RootReducer';
-import {createStore} from 'redux';
+import {createStore, Store} from 'redux';
+import * as _ from 'lodash';
 
-export let store = createStore<IRootState>(RootReducer);
+export let store: Store<IRootState>;
+
+try {
+	let preLoadedState: IRootState = JSON.parse(localStorage.getItem("state") as string);
+	store = createStore<IRootState>(RootReducer, preLoadedState);
+}
+catch(e) {
+	store = createStore<IRootState>(RootReducer);
+}
+
+let persistState = ()=>{
+	localStorage.setItem("state", JSON.stringify(store.getState()));
+};
+
+store.subscribe(_.debounce(persistState, 2000, {
+	leading: true,
+	trailing: true
+}));
