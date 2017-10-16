e6b922fefac72f22cca536819e0b5cf57676154f
Kishore <kishore.iiitn@gmail.com>
2017-10-16 11:32:17 +0530

    Differentiate, authenticated routes and unauthenticated ones.

--- App/index.tsx
+++ App/index.tsx
@@ -2,28 +2,49 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {ClassUI} from 'classui/ClassUI';
 import {Content} from 'classui/Content';
-import {NavBar} from 'classui/Navbar';
-import {Login} from './Pages/Login';
-import {Register} from './Pages/Register';
+import {NavBar, NavbarRemain} from 'classui/Navbar';
+import {Login, Register, Profile, Students} from './Pages';
+import {RequireAuthentication} from './Pages/Presentation//Login';
 import {Lesson} from './Pages/Lesson';
+import {Home} from './Pages/Home';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
-import {SocketIO} from './SocketIO';
+import {store} from './State';
+import {History_} from './History';
+import {Provider} from 'react-redux';
+
 interface IProps {};
 export class App extends React.Component<IProps, any>
 {
 	render()
 	{
-		return <BrowserRouter>
-			<ClassUI contentWidth={1024}>
-				<NavBar fixed logo="Programmer's Club"></NavBar>
-				<Content>
-					<Switch>
-					<Route path="/register" strict exact component={Register} />
-					<Route path="/login" strict exact component={Login} />
-					<Route path="/lesson" component={Lesson} />
-					</Switch>
-				</Content>
-			</ClassUI>
-		</BrowserRouter>;
+		return <Provider store={store}>
+			<BrowserRouter>
+				<ClassUI contentWidth={1024}>
+					<History_ />
+					<NavBar fixed logo="Programmer's Club">
+						<NavbarRemain/>
+						<div className="button">Profile</div>
+					</NavBar>
+					<Content>
+						<Switch>
+							<Route path="/home" strict exact component={Home} />
+							<Route path="/register" strict exact component={Register} />
+							<Route path="/login" strict exact component={Login} />
+							{/* Authenticated Components goes here...*/}
+							<Route render={(props)=>{
+								if (!store.getState().user.userid) {
+									return <RequireAuthentication message="Please login to continue." redirect={props.location.pathname}/>;
+								}
+								return <Switch>
+									<Route path="/lesson" component={Lesson} />
+									<Route path="/profile" component={Profile} />
+									<Route path="/students" component={Students} />
+								</Switch>;
+							}}/>
+						</Switch>
+					</Content>
+				</ClassUI>
+			</BrowserRouter>
+		</Provider>;
 	}
 }
