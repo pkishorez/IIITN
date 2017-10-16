7fcf51841c09c3f79b296f3a84158a6998c4a238
Kishore <kishore.iiitn@gmail.com>
2017-10-16 11:39:49 +0530

    Add a practice Question type for sample.

--- App/Pages/Presentation/Question.tsx
+++ App/Pages/Presentation/Question.tsx
@@ -34,3 +34,32 @@ export class Question extends React.Component<IQuestionProps, any> {
 		</div>;
 	}
 }
+
+interface IPracticeProps{
+	content: string
+}
+interface IPracticeState {
+	output: any
+}
+export class Practice extends React.Component<IPracticeProps, IPracticeState> {
+	constructor() {
+		super();
+		this.state = {
+			output: {}
+		}
+	}
+	render() {
+		return <div className="card-0" style={{margin: 15,padding: 10, backgroundColor: "white"}}>
+			<Monaco height={50} diffContent={{content: this.props.content}} processOutput={(output)=>this.setState({output})}/>
+			<pre className="card-0" style={{
+				padding: 10,
+				transition: "0.3s all",
+				maxHeight: 200,
+				overflow: "auto",
+				backgroundColor: "white",
+				fontFamily: "monospace",
+				color: 'black'
+			}}>{this.state.output.data}</pre>
+		</div>;
+	}
+}
