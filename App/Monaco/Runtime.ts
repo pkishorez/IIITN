4c65766111ad4047b551e404e90426660625196a
Kishore <kishore.iiitn@gmail.com>
2017-10-17 11:19:28 +0530

    Add run and runFunction variations of running code.
    Transpile code in Runtime module itself. Seperation of concerns too.

--- App/Monaco/Runtime.ts
+++ App/Monaco/Runtime.ts
@@ -1,38 +1,80 @@
 import * as _ from 'lodash';
+import {transpileModule} from './Playground';
+import * as ts from 'typescript';
 
 // POST MESSAGE
 let BUFFER_SIZE = 5;
-let processCode = (code: string)=> `
-var __kishore_bdata = "";
-console = {
-	log: function(msg) {
-		msg = msg.toString();
-		if ((__kishore_bdata.length+msg.length)>(${BUFFER_SIZE}*1024)) {
-			//self.postMessage({type: "error", data: "Buffer Overflow. Output cannot exceed 5KB."});
-			throw("Buffer Overflow. Output cannot exceed ${BUFFER_SIZE}KB.");
-			return;
+
+let Code = {
+	_before: `var __kishore_bdata = "";
+	console = {
+		log: function(msg) {
+			msg = msg.toString();
+			if ((__kishore_bdata.length+msg.length)>(${BUFFER_SIZE}*1024)) {
+				//self.postMessage({type: "error", data: "Buffer Overflow. Output cannot exceed 5KB."});
+				throw("Buffer Overflow. Output cannot exceed ${BUFFER_SIZE}KB.");
+				return;
+			}
+			__kishore_bdata += msg;
 		}
-		__kishore_bdata += msg;
+	}`,
+	_after: ``,
+	generate(code: string) {
+		// Transpile Typescript to Javascript.
+		code = transpileModule(code, {
+			module: ts.ModuleKind.AMD,
+			target: ts.ScriptTarget.ES5,
+			noLib: true,
+			noResolve: true,
+			suppressOutputPathCheck: true
+		});
+		return `
+		${this._before}
+		try {
+			${code}
+		}
+		catch(e) {
+			self.postMessage({type: "error", data: e});
+		}
+		${this._after}
+		`;
 	}
 }
-try {
-	${code}
-	self.postMessage({type: "OUTPUT", data: __kishore_bdata});
-}
-catch(e) {
-	self.postMessage({type: "error", data: e});
-}`;
 
+export interface IFunction {
+	name: string
+	testcases: {
+		input: number[]
+		output: any
+	}
+};
+export let Runtime = {
+	run(code: string, msg: Function) {
+		let genCode = Code.generate(`
+			${code};
+			self.postMessage({type: "OUTPUT", data: __kishore_bdata});
+		`);
+		runProgram(genCode, msg);
+	},
+	runFunction(code: string, func: IFunction, msg: Function) {
+		let genCode = Code.generate(`
+			${code};
+			if (${func.name}) {
+				let output = ${func.name}.apply(null, [${func.testcases.input}]);
+				self.postMessage({type: 'OUTPUT', data: output});
+			}
+			else {
+				self.postMessage({type: 'error', data: 'Function ${func.name} not defined.'});
+			}
+		`);
+		runProgram(genCode, msg);
+	}
+};
 let worker: Worker;
 
-export let throttleRunProgram = _.throttle(runProgram, 10, {
-	trailing: true,
-	leading: false
-});
-
-export function runProgram(code: string, msg: Function)
+function runProgram(code: string, msg: Function)
 {
-	let blob = new Blob([processCode(code)], {type: 'text/javascript'});
+	let blob = new Blob([code], {type: 'text/javascript'});
 	let url = URL.createObjectURL(blob);
 	let bdata = "";
 
