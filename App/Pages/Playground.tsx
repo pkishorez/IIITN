b2bf246c02418d25523821ae84720398bda0b3ea
Kishore <kishore.iiitn@gmail.com>
2017-10-21 16:41:43 +0530

    Persist playground content even at reloads.

--- App/Pages/Playground.tsx
+++ App/Pages/Playground.tsx
@@ -21,9 +21,19 @@ export class Playground extends React.Component<IProps, IState> {
 			output: ""
 		};
 		this.runProgram = _.throttle(this.runProgram.bind(this), 100);
+		this.saveContent = _.debounce(this.saveContent.bind(this), 500, {
+			trailing: true,
+			leading: true
+		});
+	}
+	saveContent(value: string) {
+		if (value){
+			localStorage.setItem("PLAYGROUND", value);			
+		}
 	}
 	runProgram(value: string)
 	{
+		this.saveContent(value);
 		let program_seq_no = output_seq+1;
 		Runtime.run(value).then((val: string)=>{
 			output_seq++;
@@ -48,9 +58,14 @@ export class Playground extends React.Component<IProps, IState> {
 
 	}
 	render() {
+		let oldContent = localStorage.getItem("PLAYGROUND");
+		let monacoprops: IMonacoProps = {
+			...this.props.monaco,
+			content: oldContent?oldContent:""
+		};
 		return <Layout align="center" style={{height: "calc(100vh - 50px)"}} gutter={10}>
 			<Section remain>
-				<Monaco {...this.props.monaco} fontSize={15} height="calc(100vh - 150px)" getOutput={this.runProgram}/>
+				<Monaco {...monacoprops} fontSize={15} height="calc(100vh - 150px)" getOutput={this.runProgram}/>
 			</Section>
 			<Section width={400} card>
 				<h3 style={{textAlign: "center"}}>Output</h3>
