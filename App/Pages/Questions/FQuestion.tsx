baaffc626c64ae2cda8a28c4a39cfe508cbb25fd
Kishore <kishore.iiitn@gmail.com>
2017-10-23 16:27:24 +0530

    Integrate SeqTestcaseOutput to FQuestion.

--- App/Pages/Questions/FQuestion.tsx
+++ App/Pages/Questions/FQuestion.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Monaco, IMonacoProps} from '../../Monaco';
 import {IFunction, Runtime} from '../../Monaco/Runtime';
-import {SeqProgramOutput} from '../../Monaco/SeqProgramOutput';
+import {SeqProgramOutput, SeqTestcaseOutput} from '../../Monaco/SeqProgram';
 import * as _ from 'lodash';
 
 export interface IFQuestionProps {
@@ -19,6 +19,7 @@ export class FQuestion extends React.Component<IFQuestionProps, IFQuestionState>
 	value: string = "";
 	throttleRun: any;
 	showOutput: SeqProgramOutput|null;
+	runTestCases: SeqTestcaseOutput|null;
 	constructor() {
 		super();
 		this.state = {
@@ -26,63 +27,22 @@ export class FQuestion extends React.Component<IFQuestionProps, IFQuestionState>
 			testCaseMessage: "",
 			output: ""
 		}
-		this.RUN = _.throttle(this.RUN.bind(this), 1000);
 		this.runProgram = this.runProgram.bind(this);
-	}
-	componentDidMount() {
-		this.RUN();
 	}
 	runProgram(value: string) {
 		if (!value)
 			return;
 		this.value = value;
 		this.showOutput && this.showOutput.runProgram(value);
-		this.RUN();
-	}
-	RUN() {
-		this.setState({
-			testCaseMessage: "Checking..."
-		})
-		Runtime.runFunction(this.value, this.props.funcDetails).then((data)=>{
-			let correct = 0;
-			let correctAnswers = _.map(this.props.funcDetails.inputs, (answer)=>(answer.output));
-			for (let i=0; i<correctAnswers.length; i++) {
-				if (data.outputs[i]==correctAnswers[i]) {
-					correct++;
-				}
-			}
-			this.setState({
-				...this.state,
-				testCases: correct,
-				testCaseMessage: "Done :)"
-			});
-		}).catch((msg)=>{
-			this.setState({
-				...this.state,
-				testCases: 0,
-				testCaseMessage: msg
-			})
-		});
+		this.runTestCases && this.runTestCases.runProgram(value);
 	}
 	render() {
 		return <div className="card-0" style={{...this.props.style, padding: 10, backgroundColor: "white"}}>
 			<div style={{padding: 10}}>
 				{this.props.children}{" "}
-				<span className="button" style={{display: 'inline-block'}} onClick={this.RUN}>Run Program</span>
 			</div>
 			<Monaco {...this.props.monaco} getOutput={this.runProgram}/>
-			<div className="card-0" style={{
-				padding: 10,
-				transition: "0.3s all",
-				backgroundColor: "white",
-				fontFamily: "monospace",
-				fontWeight: 900,
-				overflow: "auto",
-				color: (this.state.testCases==this.props.funcDetails.inputs.length)?"darkgreen":"red"				
-			}}>
-			{this.state.testCaseMessage}<br/>
-			{this.state.testCases}/{this.props.funcDetails.inputs.length} test cases passed.
-			</div>
+			<SeqTestcaseOutput throttle={500} ref={(ref)=>{this.runTestCases=ref}} funcDetails={this.props.funcDetails}/>
 			<SeqProgramOutput throttle={10} ref={(ref)=>{this.showOutput=ref}}/>
 		</div>;
 	}
