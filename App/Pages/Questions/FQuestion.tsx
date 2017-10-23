7044ec65c8be3acabcc51eb590adaa02e80d93bc
Kishore <kishore.iiitn@gmail.com>
2017-10-23 15:51:59 +0530

    Use SeqProgramOutput Component for FQuestion.

--- App/Pages/Questions/FQuestion.tsx
+++ App/Pages/Questions/FQuestion.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import {Monaco, IMonacoProps} from '../../Monaco';
 import {IFunction, Runtime} from '../../Monaco/Runtime';
+import {SeqProgramOutput} from '../../Monaco/SeqProgramOutput';
 import * as _ from 'lodash';
 
 export interface IFQuestionProps {
-	funcName: string
-	answers: {
-		input: any[],
-		output: any
-	}[]
+	funcDetails: IFunction
 	testCasesPassed?: (num: number)=>void
 	style?: React.CSSProperties
 	monaco?: IMonacoProps
@@ -16,53 +13,56 @@ export interface IFQuestionProps {
 interface IFQuestionState {
 	testCases: number
 	output: string
+	testCaseMessage: string
 }
 export class FQuestion extends React.Component<IFQuestionProps, IFQuestionState> {
 	value: string = "";
 	throttleRun: any;
+	showOutput: SeqProgramOutput|null;
 	constructor() {
 		super();
 		this.state = {
 			testCases: 0,
+			testCaseMessage: "",
 			output: ""
 		}
-		this.RUN = this.RUN.bind(this);
-		this.showOutput = _.throttle(this.showOutput.bind(this), 10);
+		this.RUN = _.throttle(this.RUN.bind(this), 1000);
 		this.runProgram = this.runProgram.bind(this);
 	}
 	componentDidMount() {
 		this.RUN();
 	}
-	showOutput() {
-		Runtime.run(this.value).then((val: string)=>{
-			console.log(val);
-			this.setState({...this.state, output: val});
-		}).catch((val)=>{console.log(val);this.setState({...this.state, output: val})});
-	}
 	runProgram(value: string) {
 		if (!value)
 			return;
 		this.value = value;
-		this.showOutput();
+		this.showOutput && this.showOutput.runProgram(value);
+		this.RUN();
 	}
 	RUN() {
-		let promises = this.props.answers.map((answer)=>{
-			return Runtime.runFunction(this.value, {name: this.props.funcName, input: answer.input});
-		});
-		let correctTestCases = 0;
-		Promise.all(promises).then((results)=>{
-			results.forEach((result, i)=>{
-				if (result==this.props.answers[i].output) {
-					correctTestCases++;
+		this.setState({
+			testCaseMessage: "Checking..."
+		})
+		Runtime.runFunction(this.value, this.props.funcDetails).then((data)=>{
+			let correct = 0;
+			let correctAnswers = _.map(this.props.funcDetails.inputs, (answer)=>(answer.output));
+			for (let i=0; i<correctAnswers.length; i++) {
+				if (data.outputs[i]==correctAnswers[i]) {
+					correct++;
 				}
-			})
+			}
 			this.setState({
 				...this.state,
-				testCases: correctTestCases
+				testCases: correct,
+				testCaseMessage: "Done :)"
+			});
+		}).catch((msg)=>{
+			this.setState({
+				...this.state,
+				testCases: 0,
+				testCaseMessage: msg
 			})
-			this.props.testCasesPassed?this.props.testCasesPassed(correctTestCases):null;
-			setTimeout(this.RUN, 2000);
-		}).catch(()=>{this.setState({...this.state, testCases: 0});setTimeout(this.RUN, 2000)});
+		});
 	}
 	render() {
 		return <div className="card-0" style={{...this.props.style, padding: 10, backgroundColor: "white"}}>
@@ -71,29 +71,19 @@ export class FQuestion extends React.Component<IFQuestionProps, IFQuestionState>
 				<span className="button" style={{display: 'inline-block'}} onClick={this.RUN}>Run Program</span>
 			</div>
 			<Monaco {...this.props.monaco} getOutput={this.runProgram}/>
-			<pre className="card-0" style={{
+			<div className="card-0" style={{
 				padding: 10,
 				transition: "0.3s all",
-				maxHeight: 100,
 				backgroundColor: "white",
 				fontFamily: "monospace",
 				fontWeight: 900,
 				overflow: "auto",
-				color: (this.state.testCases==this.props.answers.length)?"darkgreen":"red"
+				color: (this.state.testCases==this.props.funcDetails.inputs.length)?"darkgreen":"red"				
 			}}>
-			{this.state.testCases}/{this.props.answers.length} test cases passed.
-			</pre>
-			<pre className="card-0" style={{
-				padding: 10,
-				transition: "0.3s all",
-				height: 80,
-				backgroundColor: "white",
-				fontFamily: "monospace",
-				color: "black",
-				overflow: "auto"
-			}}>
-			{this.state.output}
-			</pre>
+			{this.state.testCaseMessage}<br/>
+			{this.state.testCases}/{this.props.funcDetails.inputs.length} test cases passed.
+			</div>
+			<SeqProgramOutput throttle={10} ref={(ref)=>{this.showOutput=ref}}/>
 		</div>;
 	}
 }
