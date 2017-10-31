b333697360191d22584b21f0c58228dffd798d54
Kishore <kishore.iiitn@gmail.com>
2017-10-31 13:48:14 +0530

    Move Schema to separate folder.

---
+++ Server/Database/Schema/Question.ts
@@ -0,0 +1,10 @@
+import {ISchema, ISchemaPopulate} from 'classui/Components/Form/Schema';
+
+export let S_Question: ISchema = {
+	title: {
+		type: "string"
+	},
+	description: {
+		type: "string"
+	}
+}
