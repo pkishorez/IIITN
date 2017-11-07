8df665627c4f1051a8aa1fe2688ac722eea5092f
Kishore <kishore.iiitn@gmail.com>
2017-11-07 21:05:39 +0530

    Update CompileCanvasCode.

--- App/Monaco/Runtime/canvas.ts
+++ App/Monaco/Runtime/canvas.ts
@@ -6,6 +6,7 @@ export let CompileCanvasCode = (code: string, canvasId?: string)=>{
 	
 	if (canvasId) {
 		code = code.replace(/__KISHORE_CANVAS_ID__/g, `document.getElementById('${canvasId}')`);
+		code = code.replace(/__kishore_canvaselem/g, `document.getElementById('${canvasId}')`);
 	}
 
 	console.log("COMPILED", code);
