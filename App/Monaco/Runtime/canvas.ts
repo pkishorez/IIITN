6a7e79caefef8eb4cf0f0e1c11064f7b7b929529
Kishore <kishore.iiitn@gmail.com>
2017-11-07 18:28:04 +0530

    Add compile canvas an optional parameter to change the macro to real canvas element.

--- App/Monaco/Runtime/canvas.ts
+++ App/Monaco/Runtime/canvas.ts
@@ -1,8 +1,13 @@
 import {compileCode} from './typescript';
 
-export let CompileCanvasCode = (code: string)=>{
+export let CompileCanvasCode = (code: string, canvasId?: string)=>{
 	code = code.replace(/^import [^\n]*/g, "");
 	code = code.replace(/^[\n]import [^\n]*/g, "");
 	
+	if (canvasId) {
+		code = code.replace(/__KISHORE_CANVAS_ID__/g, `document.getElementById('${canvasId}')`);
+	}
+
+	console.log("COMPILED", code);
 	return compileCode(code);
 };
