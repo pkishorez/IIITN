60ba1473122a83518653b8ada751aeb5abe73eba
Kishore <kishore.iiitn@gmail.com>
2017-11-07 17:11:08 +0530

    Correct the schema of register Form component compatible with JSONSchema.

--- App/Pages/Presentation/Register.tsx
+++ App/Pages/Presentation/Register.tsx
@@ -58,9 +58,9 @@ class RegisterComponent extends React.Component<any, {error: string, registered:
 				<TextField name="name" label="Name">Name</TextField>
 				<TextField name="email" label="Email ID">Email</TextField>
 				<TextField type="password" name="password" label="Password">Password</TextField>
-				Batch : <Select name="batch" options={(S_User.batch as any).values}></Select>
+				Batch : <Select name="batch" options={(S_User.properties as any).batch.enum}></Select>
 				<br/>
-				Branch : <Select name="branch" options={(S_User.branch as any).values}></Select>
+				Branch : <Select name="branch" options={(S_User.properties as any).branch.enum}></Select>
 				<br/>
 				<input type="submit"/>
 			</Formlayout>
