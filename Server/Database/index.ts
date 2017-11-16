db16250020e97fed2ac73b679910f7865b20c53d
Kishore <kishore.iiitn@gmail.com>
2017-11-16 14:36:30 +0530

    Update Database module to include easy way to query and update database collections.

--- Server/Database/index.ts
+++ Server/Database/index.ts
@@ -1,17 +1,109 @@
 import * as mongodb from 'mongodb';
-import {Promise} from 'es6-promise';
+import { IJSONSchema } from 'classui/Components/Form/Schema/JSONSchema';
+import { Schema } from 'classui/Components/Form/Schema';
+import * as _ from 'lodash';
 
-let db: mongodb.Db;
-export let DB = new Promise<mongodb.Db>((resolve, reject)=>{
-	if (db){
-		resolve(db);		
+class Database {
+	static get DB() {
+		return new Promise<mongodb.Db>((resolve, reject)=>{
+			mongodb.connect("mongodb://127.0.0.1:27017", (err, res)=>{
+				if (!err && res) {
+					resolve(res.db("iiitn"));					
+				}
+				reject("Couldn't connect to database.");
+			})
+			setTimeout(()=>{
+				reject("Connection to database timedout.");
+			}, 5000);
+		})
 	}
-	mongodb.connect("mongodb://127.0.0.1:27017", (err, res)=>{
-		if (err) {
-			reject(new Error("Couldn't connect to database."));
-			return;
+}
+
+export class Collection{
+	private _collection: mongodb.Collection;
+	constructor(collection: string) {
+		this.findOne = this.findOne.bind(this);
+		this.getMany = this.getMany.bind(this);
+		this.update = this.update.bind(this);
+		this.insert = this.insert.bind(this);
+		this.updateOrInsert = this.updateOrInsert.bind(this);
+		this.init = this.init.bind(this);
+
+		this.init(collection);
+	}
+	async init(collection: string) {
+		try {
+			let db = await Database.DB;
+			this._collection = db.collection(collection);
 		}
-		db = res.db("iiitn");
-		resolve(db);
-	});
-});
+		catch (e) {
+			console.error("Couldn't connect to database.");
+			process.exit();
+		}
+	}
+
+	get raw() {
+		return this._collection;
+	}
+
+	private __map(res: mongodb.Cursor) {
+		return {
+			toArray() {
+				return res.toArray();
+			},
+			toObject() {
+				let json: any = {};
+				return res.toArray().then(arr=>{
+					arr.map((elem)=>{
+						json[elem._id] = elem;
+					});
+					return json;
+				})
+			}
+		}
+	}
+	async updateOrInsert(data: any, schema?: IJSONSchema) {
+		if (data._id) {
+			return await this.update(data, schema);
+		}
+		else {
+			return await this.insert(data, schema);
+		}
+	}
+	async update(data: any, schema?: IJSONSchema) {
+		if (schema) {
+			let error = Schema.validatePartial(schema, data);
+			if (error) {
+				return Promise.reject(error);
+			}
+		}
+		try {
+			return await this._collection.updateOne({_id: data._id}, data);				
+		}
+		catch(e) {
+			throw e;
+		}
+	}
+	async insert(data: any, schema?: IJSONSchema) {
+		if (schema) {
+			let error = Schema.validate(schema, data);
+			if (error) {
+				return Promise.reject(error);
+			}
+		}
+		try {
+			return await this._collection.insertOne(data);				
+		}
+		catch(e) {
+			throw e;
+		}
+	}
+	async findOne(criteria: any, fields?: Object) {
+		return await this._collection.findOne(criteria, {fields}).catch(()=>{
+			return Promise.reject("Couldn't find Record.");
+		});
+	}
+	getMany(criteria: any, fields?: Object) {
+		return this.__map(this._collection.find(criteria, fields))
+	}
+}
