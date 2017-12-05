8c855f22e0ffe4a973f0fa203f91566017843868
Kishore <kishore.iiitn@gmail.com>
2017-12-05 14:12:54 +0530

    Add routes list and utility.

---
+++ App/_presentation/Routes/index.ts
@@ -0,0 +1,77 @@
+import {Register, Profile, Students, LearnByDoing, StarterView, StarterManagement} from 'App/Pages';
+import {Home} from 'App/Pages/Presentation/Home';
+import {RequireAuthentication} from 'App/Pages/Presentation/Login';
+import {PlaygroundTypescript, PlaygroundCanvas2D, PlaygroundCanvas2dStarter} from 'App/Pages/Playground/';
+import {Task, TaskManager} from 'App/Pages/Task/';
+import {GetState} from 'App/State';
+import {AddQuestion} from 'App/Pages/Questions';
+
+export const DEFAULT_REDIRECT_URL: string = "/playground";
+
+let RouteConfig: {
+	[id: string]: {
+		route: string
+		component: any
+	}[]
+} = {
+	"ANY": [
+		{
+			route: "/home",
+			component: Home
+		},{
+			route: "/2dplayground",
+			component: PlaygroundCanvas2D
+		},{
+			route: "/playground",
+			component: PlaygroundTypescript
+		}
+	],
+
+	"USER": [
+		{
+			route: "/task",
+			component: Task
+		},{
+			route: "/lbd",
+			component: LearnByDoing
+		},{
+			route: "/lbd/starter",
+			component: StarterView
+		}
+	],
+
+	"ADMIN": [
+		{
+			route: "/lbd/starter/manage",
+			component: StarterManagement
+		},{
+			route: "/task/manage",
+			component: TaskManager
+		}
+	]
+}
+
+type IRoles = "ANY" | "USER" | "ADMIN";
+interface IRouteDetails {
+	[id: string]: {
+		role: IRoles,
+		component: any
+	}
+}
+export let getRouteDetails = (): IRouteDetails=>{
+	let routeDetails: IRouteDetails = {};
+
+	Object.keys(RouteConfig).map((role)=>{
+		RouteConfig[role].map((route)=>{
+			routeDetails = {
+				...routeDetails,
+				[route.route as any]: {
+					role,
+					component: route.component
+				}
+			};
+		})
+	})
+
+	return routeDetails;
+}
