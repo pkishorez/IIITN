62f999d051887b37c6242317c10684973b655ba7
Kishore <kishore.iiitn@gmail.com>
2017-12-06 21:33:41 +0530

    Validate task on load of the program code.

--- App/Monaco/Tasks/Typescript/ConsoleTask.tsx
+++ App/Monaco/Tasks/Typescript/ConsoleTask.tsx
@@ -38,7 +38,7 @@ export class ConsoleTask extends React.Component<IProps, IState> {
 		super(props, context);
 		this.state = {
 			answered: false,
-			output: ""
+			output: undefined
 		};
 		this.runProgram = _.debounce(this.runProgram.bind(this), 100);
 		this.SubmitProgram = this.SubmitProgram.bind(this);
@@ -48,9 +48,16 @@ export class ConsoleTask extends React.Component<IProps, IState> {
 	}
 	runProgram(code: string="") {
 		Runtime.run(code).then((output)=>{
+			if (!this.state.output) {
+				if (output==this.props.expectedOutput) {
+					this.setState({
+						answered: true
+					});
+				}
+			}
 			this.setState({
 				output
-			});
+			});	
 		});
 	}
 	diffProgram(expectedOutput: string, actualOutput: string="") {
@@ -72,27 +79,34 @@ export class ConsoleTask extends React.Component<IProps, IState> {
 		});
 	}
 	SubmitProgram() {
-		Flash.flash((dismiss)=>{
-			return <div style={{...consoleStyle, maxWidth: 500}}>
-				{this.diffProgram(this.props.expectedOutput, this.state.output)}
-			</div>;
-		}, false, true, true, "card-5");
+		if (this.state.output!=this.props.expectedOutput){
+			Flash.flash((dismiss)=>{
+				return <div style={{...consoleStyle, maxWidth: 500}}>
+					{this.diffProgram(this.props.expectedOutput, this.state.output)}
+				</div>;
+			}, false, true, true, "card-5");
+		}
 		this.setState({
 			answered: (this.state.output==this.props.expectedOutput)
 		});
 	}
 	render() {
 		return <div style={{borderLeft: "3px solid "+(this.state.answered?"green":"red"), paddingLeft: 0, marginLeft: 0}}>
-			<div style={{backgroundColor: "black",color: 'white', fontWeight: 900, padding: 20, margin: 0}}>
-				(press ctrl + ⏎ to submit).
-			</div>
+			{this.state.answered?<Layout style={{backgroundColor: "white",color: 'black', fontWeight: 900, padding: 20, margin: 0}}>
+				<Section remain>Successfully completed the task!</Section>
+				<Section>
+					<span className="button flat" onClick={()=>this.setState({answered: false})}>
+						Edit <i className="fa fa-edit"></i>
+					</span>
+				</Section>
+			</Layout>:null}
 			{(!this.state.answered)?<div>
-				<PersistMonaco theme="vs-dark" id={this.props.eid} autoResize ctrlEnterAction={this.SubmitProgram} lineNumbers="on" noborder dimensions={{
+				<PersistMonaco theme="vs-dark" fontWeight="900" id={this.props.eid} autoResize ctrlEnterAction={this.SubmitProgram} lineNumbers="on" noborder dimensions={{
 					minHeight: 60,
 					maxHeight: 200
 				}} {...this.props.monaco} getOutput={this.runProgram}/>
 				<div style={consoleStyle} >
-					<h4 style={{marginTop: 0, color: 'white'}}>Expected Output: </h4>
+					{/*<h4 style={{marginTop: 0, color: 'white'}}>Expected Output: </h4>*/}
 					{this.props.expectedOutput}
 				</div>
 			</div>:null}
