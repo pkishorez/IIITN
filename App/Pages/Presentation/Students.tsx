4bb108c96f3d228643d64bf1de99b6ae543e1c39
Kishore <kishore.iiitn@gmail.com>
2017-12-21 13:53:41 +0530

    Display students data in Table.

--- App/Pages/Presentation/Students.tsx
+++ App/Pages/Presentation/Students.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Me} from 'App/MyActions';
 import {Layout, Section} from 'classui/Components/Layout';
+import { Table } from 'classui/Components/Table';
 
 interface IProps {};
 interface IState {
@@ -10,13 +11,11 @@ interface IState {
 
 export class Students extends React.Component<IProps, IState> {
 	componentDidMount() {
-		/*
-		Database.getStudents().then((data)=>{
+		Me.getStudents().then((data)=>{
 			this.setState({
 				students:data
 			});
 		}).catch(console.error);
-		*/
 	}
 	constructor(props: any, context: any) {
 		super(props, context);
@@ -25,18 +24,9 @@ export class Students extends React.Component<IProps, IState> {
 		}
 	}
 	render() {
-		return <Layout>
-			<Section basis={300}>
-			{
-				this.state.students.map((student: any)=>{
-					return <div className="card-1" style={{backgroundColor: "white", padding: 10, margin: 15}} key={student._id}>
-						<h4>{student.name}</h4>
-						<h5>Class : {student.batch}-{student.branch}</h5>
-						<h5>Learning Progress : 50%</h5>
-					</div>
-				})
-			}
-			</Section>
-		</Layout>;
+		return <div  style={{maxWidth: 800, margin: "auto"}}>
+			<Table hoverable groupableItems={["batch"]} sortableItems={["name","batch", "branch"]} headerItems={["_id","name", "batch", "branch"]} data={this.state.students}>
+			</Table>
+		</div>;
 	}
 };
