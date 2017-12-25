c78e769cae9b0fc2a0f7910451ceedaf33354825
Kishore <kishore.iiitn@gmail.com>
2017-12-25 21:26:09 +0530

    Enhance Students List component UI. Use Fragment syntax instead of array syntax for array of children.

--- App/Pages/Presentation/Students.tsx
+++ App/Pages/Presentation/Students.tsx
@@ -3,10 +3,12 @@ import * as ReactDOM from 'react-dom';
 import {Me} from 'App/MyActions';
 import {Layout, Section} from 'classui/Components/Layout';
 import { Table } from 'classui/Components/Table';
+import { Flash } from 'classui/Components/Flash';
 
 interface IProps {};
 interface IState {
 	students: any
+	selected: any[]
 };
 
 export class Students extends React.Component<IProps, IState> {
@@ -20,13 +22,22 @@ export class Students extends React.Component<IProps, IState> {
 	constructor(props: any, context: any) {
 		super(props, context);
 		this.state = {
-			students: []
+			students: [],
+			selected: []
 		}
 	}
 	render() {
-		return <div  style={{maxWidth: 800, margin: "auto"}}>
-			<Table hoverable groupableItems={["batch"]} sortableItems={["name","batch", "branch"]} headerItems={["_id","name", "batch", "branch"]} data={this.state.students}>
-			</Table>
-		</div>;
+		return <>
+			<div style={{maxWidth: 800, margin: "auto", paddingTop: 30}}>
+				<Table onSelect={(selected)=>{this.setState({selected})}} rowSelectable hoverable defaultGroup="batch"
+					sortableItems={["name","batch"]}
+					headerItems={["_id","name", "batch"]}
+					data={this.state.students.filter((student: any)=>student.branch=="CSE")}
+					columnUI={{
+						"name": (data)=><div style={{padding: 5}}>{data.name}</div>
+					}}>
+				</Table>
+			</div>
+		</>;
 	}
 };
